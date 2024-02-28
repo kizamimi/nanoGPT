057bf5f932c8a307bc0e423bf42c06cef5b49b24
kizamimi <kizamimi@users.noreply.github.com>
2024-02-28 15:12:18 +0900

    Change to the study settings used this time

--- config/train_T12_char.py
+++ config/train_T12_char.py
@@ -25,7 +25,7 @@ n_embd = 512
 dropout = 0.2
 
 learning_rate = 1e-3 # with baby networks can afford to go a bit higher
-max_iters = 8 * 1000 * 1000
+max_iters = 6000
 lr_decay_iters = 5000 # make equal to max_iters usually
 min_lr = 1e-4 # learning_rate / 10 usually
 beta2 = 0.99 # make a bit bigger because number of tokens per iter is small
